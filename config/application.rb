cae890fb430ffe4f82f9e161c1d23849bb18eba1
shohei <shoheimoment@gmail.com>
2020-11-12 08:44:04 +0900

    application.rbに設定を追加

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,12 @@ module Wod
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+    config.generators do |g|
+      g.assets     false
+      g.helper     false
+    end
   end
 end
