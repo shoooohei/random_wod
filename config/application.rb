485871862b149dd5a1ab0f0a7b655530afa6dfc2
shohei <shoheimoment@gmail.com>
2020-11-15 19:34:45 +0900

    一旦jaに翻訳せずに進めるため、default_localeを除去

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,6 @@ module RandomWod
     # the framework and any gems in your application.
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-    config.i18n.default_locale = :ja
     config.generators do |g|
       g.assets     false
       g.helper     false
