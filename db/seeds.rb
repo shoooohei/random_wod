ab84a13144b8cba23557b05d4d647471b5b583a2
shohei <shoheimoment@gmail.com>
2020-11-22 18:04:09 +0900

    WODデータの出力と挿入するスクリプトを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+return unless File.exist?('tmp/wod_data.json')
+wod_data = []
+File.open('tmp/wod_data.json') do |file|
+  wod_data = JSON.load(file)
+end
+
+Wod.transaction do
+  wod_data.each do |attributes|
+    wod = Wod.new(date: Date.parse(attributes["date"]), content: attributes["content"])
+    if wod.save
+      puts "#{attributes["date"]}のWODの保存に成功しました"
+    else
+      puts "#{attributes["date"]}のWODの保存に失敗しました"
+      puts "#{wod.errors.full_messages}"
+      raise ActiveRecord::Rollback
+    end
+  end
+end
