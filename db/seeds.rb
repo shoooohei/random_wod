40c3f5dba72658d20ab6d715b0eaa21577d36b5b
shohei <shoheimoment@gmail.com>
2020-11-22 18:40:47 +0900

    seedのスクリプトを修正
    WOD１件１件のログを削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,15 +4,19 @@ File.open('tmp/wod_data.json') do |file|
   wod_data = JSON.load(file)
 end
 
+is_succeeded = true
 Wod.transaction do
   wod_data.each do |attributes|
     wod = Wod.new(date: Date.parse(attributes["date"]), content: attributes["content"])
-    if wod.save
-      puts "#{attributes["date"]}のWODの保存に成功しました"
-    else
+    unless wod.save
+      is_succeeded = false
       puts "#{attributes["date"]}のWODの保存に失敗しました"
       puts "#{wod.errors.full_messages}"
       raise ActiveRecord::Rollback
     end
   end
+end
+
+if is_succeeded
+  puts "全てのWODのデータの挿入に成功しました。"
 end
