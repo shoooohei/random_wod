fdcb42314cd7abbded674f21010257f89b52310c
Kawasaki Shohei <32407398+shoooohei@users.noreply.github.com>
2021-03-02 21:37:14 +0900

    MaebashiのWODデータを取得するAPIを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   resources :instagram_wods, only: [:new, :create]
   resources :wodify_wods, only: [:new, :create]
   resources :wods, only: [:index, :show]
+  namespace :api, defaults: { format: :json} do
+    namespace :v1 do
+      post 'create_maebashi_wod', to: 'maebashi_wods#create'
+    end
+  end
   root 'wods#index'
 end
